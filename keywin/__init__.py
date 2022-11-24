7bc31fab8964c65bb9bcca9903c7e63e8d7d6d71
winstxnhdw <winstxnhdw@gmail.com>
2022-11-24 21:28:32 +0800

    refactor: abstract helper functions

--- keywin/__init__.py
+++ keywin/__init__.py
@@ -21,51 +21,42 @@ class Mouse:
         send_mouse_event(inputs)
 
 
+    def click(flags: int):
+
+        Mouse.send_events([0, 0, 0, flags])
+        
+
     def left_click():
 
-        Mouse.send_events([
-            0, 0, 0, KeyCodes.MOUSEEVENTF_LEFTDOWN | KeyCodes.MOUSEEVENTF_LEFTUP
-        ])
+        Mouse.click(KeyCodes.MOUSEEVENTF_LEFTDOWN | KeyCodes.MOUSEEVENTF_LEFTUP)
 
 
     def right_click():
 
-        Mouse.send_events([
-            0, 0, 0, KeyCodes.MOUSEEVENTF_RIGHTDOWN | KeyCodes.MOUSEEVENTF_RIGHTUP
-        ])
+        Mouse.click(KeyCodes.MOUSEEVENTF_RIGHTDOWN | KeyCodes.MOUSEEVENTF_RIGHTUP)
 
 
     def middle_click():
 
-        Mouse.send_events([
-            0, 0, 0, KeyCodes.MOUSEEVENTF_MIDDLEDOWN | KeyCodes.MOUSEEVENTF_MIDDLEUP
-        ])
+        Mouse.click(KeyCodes.MOUSEEVENTF_MIDDLEDOWN | KeyCodes.MOUSEEVENTF_MIDDLEUP)
+
+
+    def move_relative(x: int, y: int, flag: int=0):
+
+        Mouse.send_events([x, y, 0, KeyCodes.MOUSEEVENTF_MOVE | flag])
 
 
     def move(x: int, y: int):
 
-        Mouse.send_events([
-            x, y, 0, KeyCodes.MOUSEEVENTF_MOVE | KeyCodes.MOUSEEVENTF_ABSOLUTE
-        ])
+        Mouse.move_relative(x, y, KeyCodes.MOUSEEVENTF_ABSOLUTE)
 
 
-    def move_relative(x: int, y: int):
+    def scroll(scroll_delta: int, flags: int=KeyCodes.MOUSEEVENTF_WHEEL):
 
-        Mouse.send_events([
-            x, y, 0, KeyCodes.MOUSEEVENTF_MOVE
-        ])
-
-
-    def scroll(scroll_delta: int):
-
-        Mouse.send_events([
-            0, 0, scroll_delta, KeyCodes.MOUSEEVENTF_WHEEL
-        ])
+        Mouse.send_events([0, 0, scroll_delta, flags])
 
 
     def scroll_horizontal(scroll_delta: int):
 
-        Mouse.send_events([
-            0, 0, scroll_delta, KeyCodes.MOUSEEVENTF_HWHEEL
-        ])
+        Mouse.scroll(scroll_delta, KeyCodes.MOUSEEVENTF_HWHEEL)
 
