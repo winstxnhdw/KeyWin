2bd9aabebae984fe1a727dca17c984d90aa747be
winstxnhdw <winstxnhdw@gmail.com>
2022-11-25 10:35:54 +0800

    feat(mouse): add more helpers - Add press and release helpers - Add xbutton1 and xbutton2 helpers

--- keywin/mouse.py
+++ keywin/mouse.py
@@ -14,9 +14,9 @@ class Mouse:
         send_mouse_event(inputs)
 
 
-    def click(flags: int):
+    def click(flags: int, mouse_data: int=0):
 
-        Mouse.send_events([0, 0, 0, flags])
+        Mouse.send_events([0, 0, mouse_data, flags])
         
 
     def left_click():
@@ -32,6 +32,65 @@ class Mouse:
     def middle_click():
 
         Mouse.click(MouseCodes.MOUSEEVENTF_MIDDLEDOWN | MouseCodes.MOUSEEVENTF_MIDDLEUP)
+
+
+    def xbutton1_click():
+
+        Mouse.click(MouseCodes.MOUSEEVENTF_XDOWN | MouseCodes.MOUSEEVENTF_XUP, 0x0001)
+
+
+    def xbutton2_click():
+
+        Mouse.click(MouseCodes.MOUSEEVENTF_XDOWN | MouseCodes.MOUSEEVENTF_XUP, 0x0002)
+
+
+    def left_press():
+
+        Mouse.click(MouseCodes.MOUSEEVENTF_LEFTDOWN)
+
+
+    def left_release():
+
+        Mouse.click(MouseCodes.MOUSEEVENTF_LEFTUP)
+
+    def right_press():
+
+        Mouse.click(MouseCodes.MOUSEEVENTF_RIGHTDOWN)
+
+    
+    def right_release():
+            
+        Mouse.click(MouseCodes.MOUSEEVENTF_RIGHTUP)
+
+
+    def middle_press():
+
+        Mouse.click(MouseCodes.MOUSEEVENTF_MIDDLEDOWN)
+
+    
+    def middle_release():
+
+        Mouse.click(MouseCodes.MOUSEEVENTF_MIDDLEUP)
+
+
+    def xbutton1_press():
+
+        Mouse.click(MouseCodes.MOUSEEVENTF_XDOWN, 0x0001)
+
+
+    def xbutton1_release():
+
+        Mouse.click(MouseCodes.MOUSEEVENTF_XUP, 0x0001)
+
+
+    def xbutton2_press():
+
+        Mouse.click(MouseCodes.MOUSEEVENTF_XDOWN, 0x0002)
+    
+
+    def xbutton2_release():
+
+        Mouse.click(MouseCodes.MOUSEEVENTF_XUP, 0x0002)
 
 
     def move_relative(x: int, y: int, flag: int=0):
