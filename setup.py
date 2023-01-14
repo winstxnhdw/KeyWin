e6bbf866f01d4c105163eba95a29a8fd7fe0aad6
winstxnhdw <winstxnhdw@gmail.com>
2023-01-14 20:59:17 +0800

    build: convert distutils to setuptools

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
-from distutils.core import Extension, setup
+from setuptools import Extension, setup
 
-setup (
+
+setup(
     ext_modules=[
         Extension(
             'keywin.send_input',
             ['keywin/send_input/send_input.c'],
             libraries=['user32']
         )
-    ],
+    ]
 )
