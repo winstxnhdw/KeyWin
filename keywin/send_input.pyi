075b37533ff682f457aae59afb411faeb9190351
winstxnhdw <winstxnhdw@gmail.com>
2023-03-23 07:15:57 +0800

    refactor: raise `NotImplementedError` instead - prevents python from running and skipping a function if it somehow runs the interface instead of the C extension

--- keywin/send_input.pyi
+++ keywin/send_input.pyi
@@ -1,8 +1,8 @@
 # pylint: skip-file
 
 def send_mouse_event(inputs: list[list[int]]) -> None:
-    pass
+    raise NotImplementedError
 
 
 def press_keyboard(keys: list[int]) -> None:
-    pass
+    raise NotImplementedError
