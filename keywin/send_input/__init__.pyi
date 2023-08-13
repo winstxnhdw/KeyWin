28a185a76d834488ddd7dc7f569f2d935d8275ca
winstxnhdw <winstxnhdw@gmail.com>
2023-08-14 02:49:55 +0800

    chore: add documentation to the C extension stubs

--- keywin/send_input/__init__.pyi
+++ keywin/send_input/__init__.pyi
@@ -2,6 +2,27 @@
 
 from typing import Iterable
 
-def send_mouse_event(inputs: Iterable[list[int]]) -> None: ...
+def send_mouse_event(inputs: Iterable[list[int]]) -> None:
+    """
+    Summary
+    -------
+    a low-level mouse event wrapper for the Windows API SendInput function
 
-def press_keyboard(keys: Iterable[int]) -> None: ...
+    Parameters
+    ----------
+    inputs (Iterable[list[int]]) : a list of valid mouse inputs
+    """
+    ...
+
+
+def press_keyboard(keys: Iterable[int]) -> None:
+    """
+    Summary
+    -------
+    a low-level keyboard event wrapper for the Windows API SendInput function
+
+    Parameters
+    ----------
+    keys (Iterable[int]) : a list of valid keyboard inputs
+    """
+    ...
